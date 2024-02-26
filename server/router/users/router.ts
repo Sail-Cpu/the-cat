c6de4cefe8b81e13504c44c7dfd7b6c4caea4747
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2024-02-26 15:38:46 +0100

    Implement in the back the endpoint signin

--- server/router/users/router.ts
+++ server/router/users/router.ts
@@ -51,6 +51,42 @@ router.post(`/signup`, async (req: Request, res: Response) => {
             })
         } catch (error){
             return res.status(500).send({message: "An internal error has occurred on the server"})
+        }finally {
+            await prisma.$disconnect();
+        }
+    }else{
+        return res.status(400).send(
+            {message: "The server was unable to satisfy the request, information is missing"
+            })
+    }
+})
+
+router.post(`/signin`, async (req: Request, res: Response) => {
+    const { nameEmail, password } = req.body;
+
+    console.log(nameEmail, password)
+
+    if(nameEmail && password){
+        try{
+            const userExist = await prisma.users.findMany({
+                where: {
+                    OR: [
+                        {username: nameEmail},
+                        {email: nameEmail}
+                    ]
+                }
+            })
+            if(userExist.length == 0) return res.status(400).send({message: "The users does not exist"});
+            console.log(userExist[0].password);
+            bcrypt.compare(password, userExist[0].password, (err: Error | undefined, isTheSame: boolean) => {
+                if(err) throw err;
+                if(isTheSame) return res.status(200).send({data: userExist});
+                return  res.status(400).send({ message: "Incorrect password" })
+            })
+        }catch (error){
+            return res.status(500).send({message: "An internal error has occurred on the server"})
+        }finally {
+            await prisma.$disconnect();
         }
     }else{
         return res.status(400).send(
